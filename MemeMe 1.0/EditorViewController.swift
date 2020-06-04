10443a690ba0c163a1975817e53e82b73345c1fb
Kemal Can Kaynak <kcankaynak@gmail.com>
2020-06-04 19:29:30 +0200

    First commit

---
+++ MemeMe 1.0/EditorViewController.swift
@@ -0,0 +1,204 @@
+//
+//  ViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Kemal Kaynak on 04.06.20.
+//  Copyright © 2020 Kemal Kaynak. All rights reserved.
+//
+
+import UIKit
+
+class EditorViewController: UIViewController {
+
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var memeToolbar: UIToolbar!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
+    let defaultTopTextFieldText = "TOP"
+    let defaultBottomTextFieldText = "BOTTOM"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        topTextField.defaultTextAttributes = getTextFieldAttributes()
+        bottomTextField.defaultTextAttributes = getTextFieldAttributes()
+        topTextField.textAlignment = .center
+        bottomTextField.textAlignment = .center
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        setupShareButtonState()
+        setupObservers()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func setupObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func setupShareButtonState() {
+        shareButton.isEnabled = memeImageView.image != nil
+    }
+}
+
+// MARK: - IBActions -
+
+extension EditorViewController {
+    
+    @IBAction func cameraAction(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func albumAction(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func shareAction(_ sender: UIBarButtonItem) {
+        if let memeImage = generateMemedImage() {
+            let activityViewController = UIActivityViewController(activityItems: [memeImage], applicationActivities: nil)
+            activityViewController.completionWithItemsHandler = { activity, success, items, error in
+                if success {
+                    self.saveImage(memeImage)
+                }
+            }
+            present(activityViewController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func cancelAction(_ sender: UIBarButtonItem) {
+        topTextField.text = defaultTopTextFieldText
+        bottomTextField.text = defaultBottomTextFieldText
+        memeImageView.image = nil
+        setupShareButtonState()
+    }
+}
+
+// MARK: - Get UITextField Attributes -
+
+extension EditorViewController {
+    
+    func getTextFieldAttributes() -> [NSAttributedString.Key: Any] {
+        let textAttributes: [NSAttributedString.Key: Any] = [NSAttributedString.Key.strokeColor: UIColor.black,
+                                                             NSAttributedString.Key.foregroundColor: UIColor.white,
+                                                             NSAttributedString.Key.font: UIFont(name: "impact", size: 44)!,
+                                                             NSAttributedString.Key.strokeWidth: -3.0]
+        return textAttributes
+    }
+}
+
+// MARK: - UIImagePicker Delegate -
+
+extension EditorViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.editedImage] as? UIImage {
+            memeImageView.image = image
+            setupShareButtonState()
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
+}
+
+// MARK: - UITextField Delegate -
+
+extension EditorViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if isTextFieldEmpty(textField) {
+            if textField == topTextField {
+                topTextField.text = defaultTopTextFieldText
+            } else {
+                bottomTextField.text = defaultBottomTextFieldText
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func isTextFieldEmpty(_ textField: UITextField) -> Bool {
+        guard let text = textField.text, text.isEmpty else { return false }
+        return true
+    }
+}
+
+// MARK: - Keyboard Notification -
+
+extension EditorViewController {
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if bottomTextField.isEditing {
+            view.frame.origin.y -= getKeyboardHeight(notification.userInfo)
+        }
+    }
+    
+    @objc func keyboardWillHide() {
+        view.frame.origin.y = 0
+    }
+
+    func getKeyboardHeight(_ info: [AnyHashable: Any]?) -> CGFloat {
+        guard let keyboardHeight = info?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return 0.0
+        }
+        return keyboardHeight.cgRectValue.height
+    }
+}
+
+// MARK: - Generate Meme Image -
+
+extension EditorViewController {
+    
+    func generateMemedImage() -> UIImage? {
+        setupSystemBarVisibility(true)
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        if let memedImage = UIGraphicsGetImageFromCurrentImageContext() {
+            UIGraphicsEndImageContext()
+            setupSystemBarVisibility(false)
+            return memedImage
+        }
+        UIGraphicsEndImageContext()
+        setupSystemBarVisibility(false)
+        return nil
+    }
+    
+    func setupSystemBarVisibility(_ isHidden: Bool) {
+        memeToolbar.isHidden = isHidden
+        navigationController?.navigationBar.isHidden = isHidden
+    }
+}
+
+// MARK: - Save Image -
+
+extension EditorViewController {
+    
+    func saveImage(_ memedImage: UIImage) {
+        if let topText = topTextField.text, let bottomText = bottomTextField.text, let originalImage = memeImageView.image {
+            let memeModel = Meme(topText: topText, bottomText: bottomText, originalImage: originalImage, memedImage: memedImage)
+        }
+    }
+}
---
+++ MemeMe 1.0/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  MemeMe 1.0
+//
+//  Created by Kemal Kaynak on 04.06.20.
+//  Copyright © 2020 Kemal Kaynak. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
